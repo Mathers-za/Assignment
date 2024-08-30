0b7a70ae31fa5623e0a4f7d0e31b82b864dd2099
Daniel <danielmathers97@gmail.com>
2024-08-30 17:37:48 +0200

    fix: fix syntax in stringmnaip helperfn

--- src/helperfns/stringManipuationhelperFns.js
+++ src/helperfns/stringManipuationhelperFns.js
@@ -4,10 +4,8 @@ export function convertStringIntoArrayAndSortAscending(string) {
       (a, b) => a.charCodeAt(0) - b.charCodeAt(0) // this is more verbose - could also say: sort()
     );
   }
-  {
-    console.log(
-      `covertStringIntoArrayAndSortAscending fn expects a string but got a ${typeof inputString}`
-    );
-    return []; //added this code to provide a fallback and not crash the program during runtime
-  }
+  console.log(
+    `covertStringIntoArrayAndSortAscending fn expects a string but got a ${typeof inputString}`
+  );
+  return []; //added this code to provide a fallback and not crash the program during runtime
 }
